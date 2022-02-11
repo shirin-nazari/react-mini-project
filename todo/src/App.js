596939e597e467b7066dd526b2b4e27c762fe868
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-02-11 11:10:55 +0330

    fixed

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,8 +1,15 @@
 import HomePage from "./component/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageNotFound from "./component/PageNotFound";
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ todo/src/component/PageNotFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h1>dari eshtebah mizani (:</h1>
+    </div>
+  );
+};
+
+export default NotFound;
